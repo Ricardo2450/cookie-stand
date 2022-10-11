7e5d903c3b4ca923de1d2aced47d2a1e5d5cfd15
Ricardo Soto-Fabela <sotofabela@gmail.com>
2022-10-10 22:25:24 -0500

    fixed hours

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ const SalmonCookiesSales = document.getElementById('salesLocations');
 
 console.log(SalmonCookiesSales);
 
-let hours = ['6 a.m.', '7 a.m.', '8 a.m.', '9 a.m.', '10 a.m.', '11 a.m.', '12 p.m.', '1 p.m.', '2 p.m.', '3 p.m.', '4 p.m.'];
+let hours = ['6 a.m.', '7 a.m.', '8 a.m.', '9 a.m.', '10 a.m.', '11 a.m.', '12 p.m.', '1 p.m.', '2 p.m.', '3 p.m.', '4 p.m.', '5 p.m.', '6 p.m.', '7 p.m.'];
 
 let seattle = {
   name: 'Seattle',
